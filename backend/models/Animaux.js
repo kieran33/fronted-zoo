390433fe7b7eab131ed55efff0ff6b86dd038489
Kiéran <lavole.kieran@gmail.com>
2024-07-06 14:35:33 +0200

    2eme commit

--- backend/models/Animaux.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SchemaAnimal = new mongoose.Schema({
-    prenom: String,
-    nombreVues: Number
-});
-
-const AnimalModel = mongoose.model("animaux_populaires", SchemaAnimal);
-module.exports = AnimalModel;
